897dd18e09907add64652c7565d75a96515f6b15
Mirania <blakbgtr@gmail.com>
2018-11-14 17:28:59 +0000

    full project

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 require("dotenv").config();
 
 const fs = require('fs');
-const api = require("commit-to-github");
 const fpath = 'data/version.txt';
+const request = require('request');
 
 let version = {v: -1};
+
+let GETheaders = {
+    'User-Agent':       'Super Agent/0.0.1',
+    'Content-Type':     'application/x-www-form-urlencoded'
+}
+
+let POSTheaders = {
+    'User-Agent':       'Super Agent/0.0.1',
+    'Content-Type':     'application/json',
+    'Authorization':    'Bearer '+process.env.GIT_TOKEN
+}
+
+let latestSHA = "";
 
 function updateVersion() {
     const n = JSON.parse(fs.readFileSync(fpath, 'utf8'));
@@ -13,21 +26,110 @@ function updateVersion() {
 };
 
 function commitVersion() {
-    api({
-        user: process.env.USER,
-        repo: process.env.REPO,
-        token: process.env.GIT_TOKEN,
-        files: [
-            {path: fpath, content: "test"},
-        ],
-        fullyQualifiedRef : "heads/master",
-        forceUpdate: false,
-        commitMessage: "Self commit (v"+version.v+")"
-    }).then(function(res){
-        console.log("Self commit (v"+version.v+") -> successful.");
-    }).catch(function(err){
-        console.log("Self commit (v"+version.v+") -> failed.");
-    })
+
+    getLatestSHA()
+        .then( (sha) => getBaseTree(sha))
+        .then( (tree) => postToTree(tree))
+        .then( (newtree) => postToCommits(latestSHA, newtree))
+        .then( (newsha) => finishCommit(newsha))
+        .then( (url) => console.log(url+"\nFinished committing version "+version.v));
+
+}
+
+async function finishCommit(newsha) {
+    let body = {
+        "sha": newsha
+    }
+
+    let options = {
+        url: 'https://api.github.com/repos/'+process.env.USER+'/'+process.env.REPO+'/git/refs/heads/master',
+        method: "POST",
+        json: true,
+        body: body,
+        headers: POSTheaders
+    }
+
+    let r = await thenableRequest(options);
+    return r.object.url;
+}
+
+async function postToCommits(sha, newtree) {
+    let body = {
+        "parents": [sha],
+        "tree": newtree,
+        "message": "Automatic commit of version "+version.v
+    }
+
+    let options = {
+        url: 'https://api.github.com/repos/'+process.env.USER+'/'+process.env.REPO+'/git/commits',
+        method: "POST",
+        json: true,
+        body: body,
+        headers: POSTheaders
+    }
+
+    let r = await thenableRequest(options);
+    return r.sha;
+}
+
+async function postToTree(tree) {
+    let body = {
+        "base_tree": tree,
+        "tree": [
+          {
+            "path": "data/version.txt",
+            "mode": "100644",
+            "type": "blob",
+            "content": JSON.stringify(version) //commit sets this as content of file
+          }
+        ]
+      }
+
+    let options = {
+        url: 'https://api.github.com/repos/'+process.env.USER+'/'+process.env.REPO+'/git/trees',
+        method: "POST",
+        json: true,
+        body: body,
+        headers: POSTheaders
+    }
+
+    let r = await thenableRequest(options);
+    return r.sha;
+}
+
+async function getBaseTree(sha) {
+    let options = {
+        url: 'https://api.github.com/repos/'+process.env.USER+'/'+process.env.REPO+'/git/commits/'+sha,
+        method: 'GET',
+        headers: GETheaders
+    }
+
+    let r = await thenableRequest(options);
+    return JSON.parse(r).tree.sha;
+}
+
+async function getLatestSHA() {
+    let options = {
+        url: 'https://api.github.com/repos/'+process.env.USER+'/'+process.env.REPO+'/git/refs/heads/master',
+        method: 'GET',
+        headers: GETheaders
+    }
+
+    let r = await thenableRequest(options);
+    latestSHA = JSON.parse(r).object.sha;
+    return latestSHA;
+}
+
+function thenableRequest(options) {
+    return new Promise((resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (error) reject(error);
+            if (response.statusCode != 200 && response.statusCode != 201) {
+                reject('Invalid status code <' + response.statusCode + '>:\n'+JSON.stringify(response));
+            }
+            resolve(body);
+        });
+    });
 }
 
 updateVersion();
